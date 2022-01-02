e43b589eec1136ada3d0470a5b6269a51c813b8a
Łukasz Czerpak <lukasz@czerpak.eu>
2022-01-02 13:10:16 +0100

    refactor

--- commands/deleteData.go
+++ commands/deleteData.go
@@ -1,8 +1,9 @@
 package commands
 
 import (
-	"solaredge-sync/deletedata"
+	"solaredge-sync/common"
 	log "solaredge-sync/logging"
+	"solaredge-sync/solaredgedb"
 	"solaredge-sync/util"
 	"time"
 
@@ -24,10 +25,23 @@ var deleteDataCmd = &cobra.Command{
 			log.Fatalf("Invalid stop time: %v", err)
 		}
 
-		deletedata.Delete(&config, startTime, stopTime)
+		deleteData(&config, startTime, stopTime)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(deleteDataCmd)
+}
+
+func deleteData(cfg *common.AppConfig, startTime, stopTime time.Time) {
+	// <START_DATE, STOP_DATE> range is inclusive thus STOP_DATE's time must be 23:59:59
+	stopTime = time.Date(stopTime.Year(), stopTime.Month(), stopTime.Day(), 23, 59, 59, 0, stopTime.Location())
+
+	db := solaredgedb.New(cfg.Influxdb.Url, cfg.Influxdb.Token, cfg.Influxdb.Org, cfg.Influxdb.Bucket)
+	defer db.Close()
+
+	log.Infof("Period from %v to %v => deleting data",
+		startTime.Format(util.DATE_FORMAT), stopTime.Format(util.DATE_FORMAT))
+
+	db.DeleteMeasurements(cfg, startTime, stopTime)
 }
--- commands/fetchData.go
+++ commands/fetchData.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
-	"solaredge-sync/fetchdata"
+	"solaredge-sync/common"
 	log "solaredge-sync/logging"
+	"solaredge-sync/solaredgeapi"
+	"solaredge-sync/solaredgedb"
 	"solaredge-sync/util"
 	"time"
 
@@ -25,11 +27,59 @@ var fetchDataCmd = &cobra.Command{
 			log.Fatalf("Invalid stop time: %v", err)
 		}
 
-		fetchdata.Fetch(&config, startTime, stopTime)
+		fetchData(&config, startTime, stopTime)
 	},
 }
 
 func init() {
 	fetchDataCmd.Flags().BoolVar(&config.General.DeleteBeforeWrite, "delete", false, "Delete old data before writing new data")
 	rootCmd.AddCommand(fetchDataCmd)
+}
+
+func fetchData(cfg *common.AppConfig, startTime, stopTime time.Time) {
+	// <START_DATE, STOP_DATE> range is inclusive thus STOP_DATE's time must be 23:59:59
+	stopTime = time.Date(stopTime.Year(), stopTime.Month(), stopTime.Day(), 23, 59, 59, 0, stopTime.Location())
+
+	db := solaredgedb.New(cfg.Influxdb.Url, cfg.Influxdb.Token, cfg.Influxdb.Org, cfg.Influxdb.Bucket)
+	defer db.Close()
+	api := solaredgeapi.New(cfg.SolarEdge.ApiKey)
+
+	for rangeStartTime := startTime; rangeStartTime.Before(stopTime); rangeStartTime = rangeStartTime.AddDate(0, 0, 7) {
+		rangeStopTime := rangeStartTime.AddDate(0, 0, 7).Add(time.Second * -1)
+		if rangeStopTime.After(stopTime) {
+			rangeStopTime = stopTime
+		}
+
+		if !cfg.General.DeleteBeforeWrite {
+			exists, err := db.CheckIfDataExists(rangeStartTime, rangeStopTime)
+			if err != nil {
+				log.Errorf("Checking DB failed: %v", err)
+				continue
+			}
+			if exists {
+				log.Infof("Period from %v to %v => data exists in db, skipping",
+					rangeStartTime.Format(util.DATE_FORMAT), rangeStopTime.Format(util.DATE_FORMAT))
+				continue
+			}
+		}
+
+		log.Infof("Period from %v to %v => fetching data from SolarEdge API",
+			rangeStartTime.Format(util.DATE_FORMAT), rangeStopTime.Format(util.DATE_FORMAT))
+
+		data, err := api.FetchData(cfg.SolarEdge.SiteId, cfg.SolarEdge.InverterSn, rangeStartTime, rangeStopTime)
+		if err != nil {
+			log.Errorf("Error on fetching data from SolarEdge API: %v", err)
+			continue
+		}
+
+		if cfg.General.DeleteBeforeWrite {
+			log.Infof("Period from %v to %v => deleting old data, if exists",
+				rangeStartTime.Format(util.DATE_FORMAT), rangeStopTime.Format(util.DATE_FORMAT))
+			db.DeleteMeasurements(cfg, rangeStartTime, rangeStopTime)
+		}
+
+		log.Infof("Period from %v to %v => Writing %d telemetries to database",
+			rangeStartTime.Format(util.DATE_FORMAT), rangeStopTime.Format(util.DATE_FORMAT), len(data.Data.Telemetries))
+		db.WriteMeasurements(cfg, data.Data.Telemetries)
+	}
 }
