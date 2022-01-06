1c7c5a0a62524c66fc97acf98bd3b92ca1fd10a0
Łukasz Czerpak <lukasz@czerpak.eu>
2022-01-06 09:30:16 +0100

    refactor

--- solaredgedb/delete_data.go
+++ solaredgedb/delete_data.go
@@ -2,14 +2,13 @@ package solaredgedb
 
 import (
 	"context"
-	"solaredge-sync/common"
 	log "solaredge-sync/logging"
 	"time"
 )
 
-func (db *SolarEdgeDB) DeleteMeasurements(cfg *common.AppConfig, startTime, stopTime time.Time) {
+func (db *SolarEdgeDB) DeleteMeasurements(startTime, stopTime time.Time) {
 	deleteAPI := db.client.DeleteAPI()
-	err := deleteAPI.DeleteWithName(context.Background(), cfg.Influxdb.Org, cfg.Influxdb.Bucket, startTime, stopTime, "")
+	err := deleteAPI.DeleteWithName(context.Background(), db.org, db.bucket, startTime, stopTime, "")
 	if err != nil {
 		log.Errorf("Error when deleting data: %v", err)
 	}
